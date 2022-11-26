22514efa31bf426aa2d773773d402ccc0417e9c9
Nora Salehson <wn00202145@wgs.wuerth.com>
2022-11-26 11:36:32 +0100

    Changed approach to compression/decompression

--- JavaScript/JsonMessage.js
+++ JavaScript/JsonMessage.js
@@ -1,110 +1,131 @@
 export default class JsonMessage {
-    static #START_BYTES = 1;
-    static #HEADER_BYTES = 2;
-    static #SEPERATOR_BYTES = 1;
-    static #END_BYTES = 0;
-
     static manifest = null;
 
-    static compress(manifest, header, values) {
-        if(this.manifest != null && values == undefined) {
-            values = header;
+    static compress(manifest, header, payload) {
+        // allow for compress(header, payload) if manifest is set
+        if(this.manifest != null && payload == undefined) {
+            payload = header;
             header = manifest;
             manifest = this.manifest;
         }
 
-        if(typeof header == "string")
-            header = Object.entries(manifest).find(([key, value]) => value.name == header)[0];
+        const structure = this.#getManifestStructure(manifest, header);
+        const message = this.#compressStructure(manifest, structure, payload);
 
-        const structure = manifest[header];
-
-        let payload = [];
-        payload.push('*');
-        payload.push(String.fromCharCode((header >> 8) & 0xFF));
-        payload.push(String.fromCharCode(header & 0xFF));
-
-        if(structure.type == "object") {
-            payload.push(structure.properties.map((property) => {
-                if(!values.hasOwnProperty(property))
-                    return "null";
-
-                return this.#compress(values[property], ',');
-            }).join());
-        }
-        else if(structure.type == "array") {
-            payload.push(values.map((value) => {
-                return this.#compress(value, '|');
-            }).join('|'));
-        }
-
-        if(this.#END_BYTES)
-            payload.push('\0');
-
-        return payload.join('');
+        return ([ '*', String.fromCharCode((structure.header >> 8) & 0xFF), String.fromCharCode(structure.header & 0xFF) ]).concat(message).join('');
     };
 
-    static #compress(value, seperator) {
-        if(typeof value == "object")
-            return JSON.stringify(value).replace(seperator, `\\${seperator}`);
-
-        return value.toString().replace(seperator, `\\${seperator}`);
-    };
-
-    static decompress(manifest, value) {
-        if(this.manifest != null && value == undefined) {
-            value = manifest;
+    static decompress(manifest, message) {
+        // allow for decompress(message) is manifest is set
+        if(this.manifest != null && message == undefined) {
+            message = manifest;
             manifest = this.manifest;
         }
 
-        const high = value.charCodeAt(1) & 0xFF;
-        const low = value.charCodeAt(2) & 0xFF;
+        const high = message.charCodeAt(1) & 0xFF;
+        const low = message.charCodeAt(2) & 0xFF;
 
-        const header = (high << 8) | low;
+        const structure = this.#getManifestStructure(manifest, (high << 8) | low);
+        const payload = this.#decompressStructure(manifest, structure, message.substring(3));
 
-        const structure = manifest[header];
+        return payload;
+    };
 
-        let payload = value.substring(this.#START_BYTES + this.#HEADER_BYTES, value.length - this.#END_BYTES);
+    static #getManifestStructure(manifest, header) {
+        if(typeof header == "string")
+            return manifest.find((structure) => structure.key == header);
 
-        let array = [];
-        const seperator = (structure.type == "object")?(','):('|');
+        return manifest.find((structure) => structure.header == header);
+    };
 
-        for(let index = 0; index < payload.length; index++) {
-            if(index == payload.length - 1) {
-                array.push(payload);
+    static #compressStructure(manifest, structure, payload) {
+        if(structure.type == "structure")
+            structure = this.#getManifestStructure(manifest, structure.key);
 
+        switch(structure.type) {
+            case "array":
+                return this.#compressArray(manifest, structure.properties, payload);
+
+            case "object":
+                return this.#compressObject(manifest, structure.properties, payload);
+
+            case "structure":
+                return this.#compressStructure(manifest, structure.properties, payload);
+
+            default:
+                return payload;
+        };
+    };
+
+    static #compressObject(manifest, structure, payload) {
+        return structure.map((key) => {
+            if(typeof payload[key] == "string")
+                return payload[key];
+
+            return this.#compressStructure(manifest, key, payload[key.key]).replace(',', '\\,');
+        }).join(',');
+    };
+
+    static #compressArray(manifest, structure, payload) {
+        return payload.map((item) => this.#compressStructure(manifest, structure, item)).join('|');
+    };
+
+    static #decompressStructure(manifest, structure, payload) {
+        if(structure.type == "structure")
+            structure = this.#getManifestStructure(manifest, structure.key);
+
+        switch(structure.type) {
+            case "array":
+                return this.#decompressArray(manifest, structure.properties, payload);
+
+            case "object":
+                return this.#decompressObject(manifest, structure.properties, payload);
+
+            case "structure":
+                return this.#decompressStructure(manifest, structure.properties, payload);
+
+            default:
+                return payload;
+        };
+    };
+
+    static #decompressObject(manifest, structure, payload) {
+        const sections = this.#decompressSections(payload, ',');
+
+        const object = {};
+
+        structure.forEach((key, index) => {
+            if(typeof key == "string")
+                return object[key] = sections[index].replace('\\,', ',');
+
+            object[key.key] = this.#decompressStructure(manifest, key, sections[index].replace('\\,', ','));
+        });
+
+        return object;
+    };
+
+    static #decompressArray(manifest, structure, payload) {
+        return this.#decompressSections(payload, '|').map((item) => this.#decompressStructure(manifest, structure, item.replace('\\|', '|')));
+    };
+
+    static #decompressSections(payload, seperator) {
+        const sections = [];
+
+        for(let index = 0; index <= payload.length; index++) {
+            if(index == payload.length) {
+                sections.push(payload);
+           
                 break;
             }
 
-            if(payload[index] != seperator || payload[index - 1] == '\\')
-                continue;
+            if(payload[index] == seperator && payload[index - 1] != '\\') {
+                sections.push(payload.substring(0, index));
 
-            array.push(payload.substring(0, index).replace(`\\${seperator}`, seperator));
-
-            payload = payload.substring(index + 1);
-            index = 0;
+                payload = payload.substring(index + 1, payload.length);
+                index = 0;
+            }
         }
 
-        array = array.map((property) => {
-            if(property[0] == '*')
-                return this.decompress(manifest, property);
-
-            return property;
-        });
-
-        if(structure.type == "object") {
-            const object = {};
-
-            structure.properties.forEach((property, index) => {
-                object[property] = array[index];
-            });
-
-            return object;
-        }
-        
-        if(structure.type == "array") {
-            return array;
-        }
-
-        return null;
+        return sections;
     };
 };
